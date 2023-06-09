debb023565273ed322b5687b94a88c11e320a17f
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-09 12:55:41 +0200

    asd

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -3,6 +3,8 @@ const { getNamedAccounts, ethers } = require("hardhat");
 //TODO get ABI✅ + Contract Address✅
 // address: 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
 
+const AMOUNT = ethers.utils.parseEther("0.02");
+
 async function getWeth() {
     const { deployer } = await getNamedAccounts();
 
@@ -11,6 +13,10 @@ async function getWeth() {
         "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
         deployer
     );
+    const tx = await iWeth.deposit({ value: AMOUNT });
+    await tx.wait(1);
+    const wethBalance = await iWeth.balanceOf(deployer);
+    console.log(`WETH balance: ${ethers.utils.formatEther(wethBalance)}`);
 }
 
 module.exports = { getWeth };
