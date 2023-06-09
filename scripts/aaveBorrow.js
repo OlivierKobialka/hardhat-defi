a4aad931b07188d6480fe516271c4b50cd4094e1
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-09 14:13:33 +0200

    AAVE

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -8,7 +8,20 @@ async function main() {
     // Lending Pool: ^
 }
 
-async function getLendingPool() {}
+async function getLendingPool(account) {
+    const lendingPoolAddressProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        account
+    );
+    const lendingPoolAddress =
+        await lendingPoolAddressProvider.getLendingPool();
+    const lendingPool = await ethers.getContractAt(
+        "ILendingPool",
+        lendingPoolAddress,
+        account
+    );
+}
 
 main()
     .then(() => process.exit(0))
