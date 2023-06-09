ac2cc171b8f022e5eb5a65bbc31a6c275483fcd3
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-09 12:50:38 +0200

    iWeth

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -1,9 +1,16 @@
-const { getNamedAccounts } = require("hardhat");
+const { getNamedAccounts, ethers } = require("hardhat");
 
-//TODO get ABI✅ + Contract Address
+//TODO get ABI✅ + Contract Address✅
+// address: 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
 
 async function getWeth() {
     const { deployer } = await getNamedAccounts();
+
+    const iWeth = await ethers.getContractAt(
+        "IWETH",
+        "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+        deployer
+    );
 }
 
 module.exports = { getWeth };
